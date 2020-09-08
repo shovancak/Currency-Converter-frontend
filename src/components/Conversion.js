21ad939c060c6f1f36558fec179bd93f002222c8
shovancak <s.hovancak@gmail.com>
2020-09-08 17:03:46 +0200

    Adding Loading element.

--- src/components/Conversion.js
+++ src/components/Conversion.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import Loading from "./Loading";
+
 import "./Conversion.css";
 
 const Conversion = () => {
@@ -12,6 +14,7 @@ const Conversion = () => {
     totalUsd: 0,
     totalConversions: 0,
   });
+  const [loading, setLoading] = useState(true);
 
   const inputValueHandler = (event) => {
     const value = event.target.value;
@@ -22,18 +25,21 @@ const Conversion = () => {
   useEffect(() => {
     //Function for getting supported currency names
     const getDestinationCurrencyOptions = async () => {
+      setLoading(true);
       try {
         const response = await fetch(
           "http://localhost:5000/api/currency/currency-data"
         );
         const data = await response.json();
         setDestinationCurrencyOptions([...Object.keys(data.latestData)]);
+        setLoading(false);
       } catch (err) {
         console.log(err);
       }
     };
     //Function for getting total stats => total USD converted, total conversion requests made
     const getTotalStats = async () => {
+      setLoading(true);
       try {
         const response = await fetch("http://localhost:5000/api/total-stats/");
         const data = await response.json();
@@ -41,6 +47,7 @@ const Conversion = () => {
           totalUsd: data.totalData.totalUsd,
           totalConversions: data.totalData.totalConversions,
         });
+        setLoading(false);
       } catch (err) {
         console.log(err);
       }
@@ -52,40 +59,46 @@ const Conversion = () => {
 
   return (
     <div className="conversion">
-      <div className="conversion-row first">
-        <div>
-          <h2>From USD</h2>
-          <h5>Add amount to convert:</h5>
-          <input
-            type="number"
-            min="0"
-            step="0.01"
-            name="amountFrom"
-            placeholder="Amount in USD"
-            onChange={inputValueHandler}
-          ></input>
-        </div>
-        <button disabled={!amountFrom || amountFrom === 0}>CONVERT</button>
-        <div>
-          <h2>Destination Currency</h2>
-          <h5>Pick destination currency:</h5>
-          <select>
-            {destinationCurrencyOptions.map((currencyOption) => (
-              <option key={currencyOption} value={currencyOption}>
-                {currencyOption}
-              </option>
-            ))}
-          </select>
-        </div>
-      </div>
-      <div className="conversion-row second">
-        <h2>Converted Amount</h2>
-        <h1>500 EUR</h1>
-      </div>
-      <div className="conversion-row third">
-        <h3>Total USD converted: {totalStats.totalUsd}</h3>
-        <h3>Total conversions made: {totalStats.totalConversions}</h3>
-      </div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <React.Fragment>
+          <div className="conversion-row first">
+            <div>
+              <h2>From USD</h2>
+              <h5>Add amount to convert:</h5>
+              <input
+                type="number"
+                min="0"
+                step="0.01"
+                name="amountFrom"
+                placeholder="Amount in USD"
+                onChange={inputValueHandler}
+              ></input>
+            </div>
+            <button disabled={!amountFrom || amountFrom === 0}>CONVERT</button>
+            <div>
+              <h2>Destination Currency</h2>
+              <h5>Pick destination currency:</h5>
+              <select>
+                {destinationCurrencyOptions.map((currencyOption) => (
+                  <option key={currencyOption} value={currencyOption}>
+                    {currencyOption}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className="conversion-row second">
+            <h2>Converted Amount</h2>
+            <h1>500 EUR</h1>
+          </div>
+          <div className="conversion-row third">
+            <h3>Total USD converted: {totalStats.totalUsd}</h3>
+            <h3>Total conversions made: {totalStats.totalConversions}</h3>
+          </div>
+        </React.Fragment>
+      )}
     </div>
   );
 };
